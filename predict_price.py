a4c380981ddef49623980547effc629c3ae762d7
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-02-29 22:32:17 +0300

    .

--- predict_price.py
+++ predict_price.py
@@ -1,11 +1,28 @@
-import numpy as np
-from pprint import pprint
 import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
-import matplotlib.pyplot as plt
 
-ts = TimeSeries(key='ALPHAVANTAGE_API_KEY', output_format='pandas')
-data, meta_data = ts.get_weekly(symbol='MSFT')
-#pprint(data.head(2))
-print(data)
+def get_symbols():
+    #May have usage limit
+    #We return sample for now 
+    
+    """
+    API_KEY = 'OQMQKBKE8VKUOZ2U'
+    
+    #This gets all listed nasdaq symbols
+    CSV_URL = "https://www.alphavantage.co/query?function=LISTING_STATUS&state={}&apikey={}"
+    
+    listed = pd.read_csv(CSV_URL.format('listed', API_KEY))
+    listed_symbols = listed['symbol'].tolist()
+    
+    
+    delisted = pd.read_csv(CSV_URL.format('delisted', API_KEY))
+    delisted_symbols = delisted['symbol'].tolist()
+    
+    print(listed_symbols)
+    """
 
+    listed = ['AAPL', 'AAPU', 'AAPX', 'AAPY', 'AAT', 'AAU', 'AAXJ', 'AB', 'ABAT', 'ABBV', 'ABCB', 'ABCL', 'ABCS', 'ABEO', 'ABEQ', 'ABEV', 'ABG', 'ABIO', 'ABL', 'ABLLL', 'ABLLW']
+    delisted = ['TINY', 'TIOA', 'TIOAU', 'TIOAW', 'TIPD', 'TIPL', 'TIS', 'TISA', 'TIVO', 'TKF', 'TKKSR', 'TKKSU', 'TKMR', 'TKPPY', 'TLC', 'TLCVF', 'TLDH']
+
+    return listed, delisted
+ 
+get_symbols()
