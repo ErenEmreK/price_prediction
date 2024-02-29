82d9ce8fdd8dd552b10c17d8f50410c53880e648
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-02-29 18:24:32 +0300

    .

--- predict_price.py
+++ predict_price.py
@@ -1,4 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from pprint import pprint
 import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
 
+ts = TimeSeries(key='ALPHAVANTAGE_API_KEY', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+#pprint(data.head(2))
+print(data.head(2))
