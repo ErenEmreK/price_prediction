7ae6ca6961b545f9239efc1b0f022b7d297199b2
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-02-29 23:41:16 +0300

    .

--- predict_price.py
+++ predict_price.py
@@ -1,28 +1,32 @@
 import pandas as pd
 
 def get_symbols():
-    #TODO May have usage limit, so save symbols to a file
-    #We return sample for now 
-    
-    """
-    API_KEY = 'OQMQKBKE8VKUOZ2U'
-    
-    #This gets all listed nasdaq symbols
-    CSV_URL = "https://www.alphavantage.co/query?function=LISTING_STATUS&state={}&apikey={}"
-    
-    listed = pd.read_csv(CSV_URL.format('listed', API_KEY))
-    listed_symbols = listed['symbol'].tolist()
-    
-    
-    delisted = pd.read_csv(CSV_URL.format('delisted', API_KEY))
-    delisted_symbols = delisted['symbol'].tolist()
-    
-    print(listed_symbols)
-    """
+    #TODO data arranging
+    listed_df = pd.read_csv('data\listing_status.csv')
+    listed_list = listed_df['sy']    
 
+"""
     listed = ['AAPL', 'AAPU', 'AAPX', 'AAPY', 'AAT', 'AAU', 'AAXJ', 'AB', 'ABAT', 'ABBV', 'ABCB', 'ABCL', 'ABCS', 'ABEO', 'ABEQ', 'ABEV', 'ABG', 'ABIO', 'ABL', 'ABLLL', 'ABLLW']
     delisted = ['TINY', 'TIOA', 'TIOAU', 'TIOAW', 'TIPD', 'TIPL', 'TIS', 'TISA', 'TIVO', 'TKF', 'TKKSR', 'TKKSU', 'TKMR', 'TKPPY', 'TLC', 'TLCVF', 'TLDH']
 
     return listed, delisted
- 
-get_symbols()
+"""
+
+def listing_status_request():
+    #This fn will get current listing status csv file
+    #We dont need right now
+    import requests
+
+    path = 'data\listing_status.csv'
+    
+    #Can replace "demo" with or API key, though not necessary
+    CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={}'
+    response = requests.get(CSV_URL.format("demo"))
+      
+    if response.status_code == 200:
+        with open(path, 'wb') as f:
+            f.write(response.content)
+        print("Listing Status CSV file saved successfully.")
+    else:
+        print("Failed to fetch data from the URL.")
+        
