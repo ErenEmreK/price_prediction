ac1f9473c27fd426689834b9794ca6a29a67fff9
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-03-01 17:17:21 +0300

    .

--- predict_price.py
+++ predict_price.py
@@ -1,16 +1,5 @@
 import pandas as pd
 
-def get_symbols():
-    #TODO data arranging
-    listed_df = pd.read_csv('data\listing_status.csv')
-    listed_list = listed_df['sy']    
-
-"""
-    listed = ['AAPL', 'AAPU', 'AAPX', 'AAPY', 'AAT', 'AAU', 'AAXJ', 'AB', 'ABAT', 'ABBV', 'ABCB', 'ABCL', 'ABCS', 'ABEO', 'ABEQ', 'ABEV', 'ABG', 'ABIO', 'ABL', 'ABLLL', 'ABLLW']
-    delisted = ['TINY', 'TIOA', 'TIOAU', 'TIOAW', 'TIPD', 'TIPL', 'TIS', 'TISA', 'TIVO', 'TKF', 'TKKSR', 'TKKSU', 'TKMR', 'TKPPY', 'TLC', 'TLCVF', 'TLDH']
-
-    return listed, delisted
-"""
 
 def listing_status_request():
     #This fn will get current listing status csv file
@@ -20,8 +9,8 @@ def listing_status_request():
     path = 'data\listing_status.csv'
     
     #Can replace "demo" with or API key, though not necessary
-    CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={}'
-    response = requests.get(CSV_URL.format("demo"))
+    url = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={}'
+    response = requests.get(url.format("demo"))
       
     if response.status_code == 200:
         with open(path, 'wb') as f:
@@ -30,3 +19,25 @@ def listing_status_request():
     else:
         print("Failed to fetch data from the URL.")
         
+def get_symbols():
+    #TODO data arranging, return listed and delisted company symbols as lists
+    #df = pd.read_csv('data\listing_status.csv')
+       
+    #we are gonna return small sample for now
+
+    listed = ['AAPL', 'AAPU', 'AAPX', 'AAPY', 'AAT', 'AAU', 'AAXJ', 'AB', 'ABAT', 'ABBV', 'ABCB', 'ABCL', 'ABCS', 'ABEO', 'ABEQ', 'ABEV', 'ABG', 'ABIO', 'ABL', 'ABLLL', 'ABLLW']
+    delisted = ['TINY', 'TIOA', 'TIOAU', 'TIOAW', 'TIPD', 'TIPL', 'TIS', 'TISA', 'TIVO', 'TKF', 'TKKSR', 'TKKSU', 'TKMR', 'TKPPY', 'TLC', 'TLCVF', 'TLDH']
+
+    return listed, delisted
+
+def get_data(symbols):
+    for symbol in symbols:
+        url = f''
+
+
+
+
+def main():
+    l_symbols, d_symbols = get_symbols()
+    
+
