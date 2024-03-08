e2340f253f978d1a7cf34b6a7365a7975257db9f
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-03-09 01:13:33 +0300

    .

--- predict.py
+++ predict.py
@@ -38,3 +38,24 @@ def create_sequences(scaled_X, scaled_y, timestep):
         y.append(scaled_y[i + timestep]) #Using next timesteps close data as target
     
     return np.array(X), np.array(y)
+
+def advisable(test_predictions, test_targets):
+    #very primitive function to assess advices on ups and downs
+    correct_ups = 0
+    wrong_ups = 0
+    correct_downs = 0
+    wrong_downs = 0
+    
+    for i in range(1, len(test_predictions)):
+        if test_predictions[i] > test_predictions[i-1]:
+            if test_targets[i] > test_targets[i-1]:
+                correct_ups += 1 
+            else: 
+                wrong_ups += 1
+        else:
+            if test_targets[i] > test_predictions[i-1]:
+                wrong_downs += 1
+            else:
+                correct_downs += 1
+    #returns (efficiency regarding ups, efficiency regarding downs)
+    return correct_ups / (correct_ups + wrong_ups), correct_downs / (correct_downs + wrong_downs)
