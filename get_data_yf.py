70daa4321f77a82c60dbd18e2e370f468ec7e9e8
ErenEmre <156677294+ErenEmreK@users.noreply.github.com>
2024-03-10 16:49:32 +0300

    .

--- get_data_yf.py
+++ get_data_yf.py
@@ -1,24 +1,19 @@
 import yfinance as yf
 import csv 
 import pandas as pd
+import os
 INTERVAL = '1wk'
 attributes_folder_path = 'data/yf_weekly'
 recommendation_data_folder_path = 'data/recommendation_data'
 
 def get_symbols():
-    #TODO get all available symbols from yahoo/ listing_status.csv
-    #TODO save symbols to reduce api usage
-    #TODO get only listed symbols
-    
-    df = pd.read_csv('data/listing_status.csv')
-    symbols = df[['symbol']].items()
-    print(symbols)
-    
-    #TODO GET ALL
+    #Gets all symbols in listing status file
+    df = pd.read_csv('data/listing_status2.csv')
+    symbols = df['symbol'].tolist()
+    return symbols
 
-def get_symbols_sample():
-    
-    #we return small set for now
+def get_symbols_sample(): 
+    #returns a small sample set
     symbols = ['AAPL', 'MSFT', 'XWEL', 'YJ', 'YHGJ', 'ZEUS', 'XTLB', 'IBM',
                'NFLX', 'V', 'CRM', 'DIS', 'TSLA', 'BA', 'AMZN', 'CRM', 'FB',
                'INTC', 'BABA', 'HD', 'PYPL', 'NKE', 'XOM', 'CSCO', 'MRK', 'UNH']
@@ -26,15 +21,17 @@ def get_symbols_sample():
     return symbols
     
 def write_recommendation_data(recommendation_data_folder_path):
-    symbols = get_symbols_sample()
+    #Writing all to seperate files is not so memory efficient
+    #But it comes handy in error situations
+    symbols = get_symbols()
     for symbol in symbols:
         recommendations = yf.Ticker(symbol).recommendations
         if recommendations.empty:
             print(f"Couldn't get recommendations for {symbol}.")
             continue
-        recommendations.to_csv(f'{recommendation_data_folder_path}/{symbol}_rec.csv', index=False)
+        recommendations.to_csv(f'{recommendation_data_folder_path}/{symbol}.csv', index=False)
     print("Recommendations have been written.")
-
+    
 def get_data(symbols):
     for symbol in symbols:
         hist = yf.download(symbol, period='max', interval=INTERVAL)
@@ -44,14 +41,19 @@ def get_data(symbols):
             print(f"Couldn't find data for {symbol}.") 
     print("Complete.")
 
-def main():
-    symbols = get_symbols()
-    get_data(symbols)
-    
-if __name__ == '__main__':
-    #main()
-    pass
+def get_available_symbols():
+    #Gets only the data we could wetch in our last attempt
+    #So it gets rid of the data which don't give recommendations 
+    folder = 'data/recommendation_data'
+    files = os.listdir(folder)
+    symbols = []
+    for filename in files:
+        if filename.endswith('.csv'):
+            symbols.append(os.path.splitext(filename)[0])
+    return symbols
 
+if __name__ == '__main__':
+    write_recommendation_data(recommendation_data_folder_path)
 
 
 
